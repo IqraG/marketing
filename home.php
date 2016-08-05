dcdd8e4659984750e0a93cb323115b4e965bb4d4
IqraG <iqra.ghazi@mail.utoronto.ca>
2016-08-05 14:55:55 -0400

    Tried to make homepage sticky only

--- home.php
+++ home.php
@@ -16,19 +16,17 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php
+				if (is_home()) { //referenced from Codex
+					$args = array(
+						'posts_per_page' => 1,
+						'post__in'  => get_option( 'sticky_posts' ),
+						'ignore_sticky_posts' => 1
+					);
+					$query = new WP_Query( $args );
+				} 
+				?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
