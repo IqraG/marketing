93a3555a562f9992ee6efd0a36c33dae81c97291
IqraG <iqra.ghazi@mail.utoronto.ca>
2016-08-05 17:26:45 -0400

    Started styling top menu

--- home.php
+++ home.php
@@ -15,9 +15,10 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main" role="main"> 
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<?php
+				/*
 				if (is_home()) { //referenced from Codex
 					$args = array(
 						'posts_per_page' => 1,
@@ -27,6 +28,18 @@ get_header(); ?>
 					$query = new WP_Query( $args );
 				} 
 				?>
+				<?php */
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
