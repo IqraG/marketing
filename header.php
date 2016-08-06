6e870c6c1ecc14b8c31cddb8bb60f5cabcf6daa1
IqraG <iqra.ghazi@mail.utoronto.ca>
2016-08-06 15:48:09 -0400

    Changed site-description
    Made site-description change classes like site-title

--- header.php
+++ header.php
@@ -29,8 +29,10 @@
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-description"><?php echo $description;?></p>
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class"site-description"><?php echo $description;?></p>
 			<?php
 			endif;
 
