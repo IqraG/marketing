097a8f742ae08f6c372250aeba289406e8376bdf
IqraG <iqra.ghazi@mail.utoronto.ca>
2016-08-07 15:34:58 -0400

    Enqueue Google Fonts

--- functions.php
+++ functions.php
@@ -108,6 +108,8 @@ function marketing_scripts() {
 
 	wp_enqueue_script( 'marketing-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_enqueue_style( 'marketing-google-fonts', 'http://fonts.googleapis.com/css?family=Quicksand|Julius+Sans+One|Poiret+One|Parisienne|Niconne|Reem+Kufi');
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
