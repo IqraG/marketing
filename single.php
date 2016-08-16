a15c7b0c4818f01f46700041a8a7c51ab7e85327
IqraG <iqra.ghazi@mail.utoronto.ca>
2016-08-16 17:17:27 -0400

    moved sidebar to left

--- single.php
+++ single.php
@@ -7,7 +7,8 @@
  * @package Marketing
  */
 
-get_header(); ?>
+get_header(); 
+get_sidebar(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -31,5 +32,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
